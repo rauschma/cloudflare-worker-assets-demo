63cfa12964a5dd2a698595d7b184efb985d645b5
Axel Rauschmayer <axel@rauschma.de>
2025-10-28 21:02:45 +0100

    Handle missing files

--- worker/index.js
+++ worker/index.js
@@ -1,9 +1,39 @@
 import { WorkerEntrypoint } from 'cloudflare:workers';
 
 export default class extends WorkerEntrypoint {
-  async fetch(_request) {
-    // https://developers.cloudflare.com/workers/static-assets/binding/#fetch
-    const assetResponse = await this.env.ASSETS.fetch('https://assets.local/index.html');
-    return assetResponse;
+  async fetch(request) {
+    const { pathname } = new URL(request.url);
+    if (pathname === '/') {
+      // https://developers.cloudflare.com/workers/static-assets/binding/#fetch
+      const assetResponse = await this.env.ASSETS.fetch('https://assets.local/index.html');
+      return assetResponse;
+    } else {
+      const lines = [
+        `<!DOCTYPE html>`,
+        `<meta charset="UTF-8">`,
+        `<title>File not found: ${escapeForHtml(pathname)}</title>`,
+        `<h1>File not found: <code>${escapeForHtml(pathname)}</code></h1>`,
+        `<a href="/">Go to start page</a>`
+      ];
+      return new Response(
+        lines.join('\n'),
+        {
+          headers: {
+            'content-type': 'text/html',
+          },
+        }
+      );
+    }
   }
+}
+
+function escapeForHtml(text) {
+  return text
+    .replace(/&/g, '&amp;') // first!
+    .replace(/>/g, '&gt;')
+    .replace(/</g, '&lt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/`/g, '&#96;')
+    ;
 }
